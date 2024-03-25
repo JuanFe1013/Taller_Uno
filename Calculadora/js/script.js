b2c58220e8bf14de907968e50b1d683b4a9cad8c
JuanFe1013 <jfquintero@uniboyaca.edu.co>
2024-03-25 18:37:57 -0500

    Creacion de la calculadora

---
+++ Calculadora/js/script.js
@@ -0,0 +1,25 @@
+let buttons = document.querySelector('.buttons');
+let btn = document.querySelectorAll('span');
+let value = document.getElementById('value');
+let toggleBtn = document.querySelector('.toggleBtn');
+let body = document.querySelector('body');
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function () {
+        if (this.innerHTML == '=') {
+            try {
+                value.innerHTML = eval(value.innerHTML);
+            } catch {
+                value.innerHTML = "Error";
+            }
+        } else if (this.innerHTML == 'Clear') {
+            value.innerHTML = "";
+        } else {
+            value.innerHTML += this.innerHTML;
+        }
+    });
+}
+
+toggleBtn.onclick = function () {
+    body.classList.toggle('dark');
+};
